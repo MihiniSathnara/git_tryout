6c16d7ca762a2e853138f77a7f8bb913165b73c3
MIHINI SATHNARA <smihinis@gmail.com>
2024-12-02 16:13:16 +0530

    First commit

---
+++ project.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class project{
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter 1st character: ");
+		char char1=scan.next().charAt(0);
+		System.out.print("Enter 2nd character: ");
+		char char2=scan.next().charAt(0);
+		System.out.print("Enter the number of the lines: ");
+		int lineNo=scan.nextInt();
+		
+		for(int i=1;i<=lineNo;++i){
+			for(int j=1;j<(i+1);j++){
+				if(j%2!=0){
+					System.out.print(char1);
+				}else{
+					System.out.print(char2);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
